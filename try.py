b214847f8d525c26ce3d9c8e3943d21052f6acfb
wzy <421503262@qq.com>
2024-01-21 22:04:35 +0800

    oop

--- try.py
+++ try.py
@@ -1,3 +1,48 @@
-a=[]
-a.append(1)
-print(a)
+class Item:
+    pay_rate=0.8
+    all = []
+    def __init__(self, name:str ,price: float,quantity=0):
+         
+         assert price>=0, f"Price {price} is not greater than zero"
+         assert quantity>=0
+
+         self.name=name
+         self.price=price
+         self.quantity=quantity
+         
+
+         Item.all.append(self)
+
+    def calculate_total_price(self):
+        return self.price*self.quantity
+
+    def apply_discount(self):
+        self.price=self.price*self.pay_rate
+
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
+    
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader=csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
+
+Item.instantiate_from_csv()
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+#for instance in Item.all:
+    #print(instance.name)
+
+print(Item.all)
